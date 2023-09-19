f51a6cb9cd16d88806b1a3bdc0516727c18b52e5
ArunIT22 <arun.it.trainer@gmail.com>
2023-09-19 17:28:49 +0530

    Add project files.

---
+++ ClassLibrary1/EmployeeService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassLibrary1
+{
+    public class EmployeeService
+    {
+        private readonly Employee _employee = null;
+
+        public EmployeeService()
+        {
+            _employee = new Employee();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _employee.GetEmployees();
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return _employee.GetEmployees().FirstOrDefault(e => e.Id == id);
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            if (employee != null)
+            {
+                _employee.GetEmployees().Add(employee);
+            }
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            var searchedEmployee = GetEmployee(id);
+            if(searchedEmployee != null)
+            {
+                _employee.GetEmployees().Remove(searchedEmployee);
+            }
+        }
+    }
+}
