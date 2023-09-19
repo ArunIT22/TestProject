3b7caf4100db129bbbaaf5481c86c369e758fb8e
ArunIT22 <arun.it.trainer@gmail.com>
2023-09-19 17:43:14 +0530

    update

--- SpecFlowProject1/StepDefinitions/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SpecFlowProject1.StepDefinitions
-{
-    public class Product
-    {
-        public string ProductName { get; set; }
-        public decimal UnitPrice { get; set; }
-        public int UnitsInStock { get; set; }
-    }
-}
